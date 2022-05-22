d4b227819e786fad3137874a3a0eb044f59029cf
Netanel Yesharim <netanelry@yahoo.com>
2022-05-22 13:19:19 +0300

    Add project files.

---
+++ WebApplication3/Dal.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace WebApplication3
+{
+    public class Dal
+    {
+        //טעינה
+
+        //הנגשה
+        public List<user> users { get; set; }
+
+        // Singleton
+
+        private static Dal dal;
+
+        public static Dal GetInstance()
+        {
+            if (dal == null) dal = new Dal();
+            return dal;
+        }
+
+
+        private Dal()
+        {
+            //ctor, data members, 
+
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync("http://jsonplaceholder.typicode.com/todos").Result;
+
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+
+            users = Newtonsoft.Json.JsonConvert.DeserializeObject<List<user>>(responseBody);
+
+        }
+    }
+
+    public class user
+    {
+
+        public user()
+        {
+
+        }
+
+        public int userId {get; set;}
+        public string title { get; set; }
+        public int id { get; set; }
+        public bool completed { get; set; }
+
+    }
+}
---
+++ WebApplication3/ValuesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController()
+        { 
+        
+        }
+
+        [Route("search")]
+        [HttpGet]
+        public IActionResult Search(string term)
+        {
+            // lazy loading
+            Dal Dal = Dal.GetInstance();
+            List<user> results = new List<user>();
+            foreach (user user in Dal.users)
+            {
+                if ((user.title.ToLower().Contains(term.ToLower()) || (user.title.ToLower().Contains(term.ToLower()))))
+                {
+                    results.Add(user);
+                }
+            }
+            return Ok(results);
+        }
+
+        [Route("id")]
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            Dal Dal = Dal.GetInstance();
+            user result = null;
+            foreach (user user in Dal.users)
+            {
+                if (user.id == id)
+                {
+                    return Ok(user);
+                }
+            }
+            return NotFound("החיפוש שלך לא הניב תוצאות");
+        }
+
+        [Route("searchRange")]
+        [HttpGet]
+        public IActionResult SearchPriceRange(int minRange, double maxRange)
+        {
+            Dal Dal = Dal.GetInstance();
+            List<user> results = new List<user>();
+            foreach (user user in Dal.users)
+            {
+                if ((user.id >= minRange) && (user.id <= maxRange))
+                {
+                    results.Add(user);
+                }
+            }
+            return Ok(results);
+        }
+
+        //עמוד swagger?
+    }
+}
